f0a6e069c380e0ee4fd77a5ed7086ce56450b9ea
Kaua-Vidal <kaua.vidal@dcx.ufpb.br>
2025-07-07 22:33:28 -0300

    Learning about Variable

---
+++ JavaScript/variaveis.js
@@ -0,0 +1,12 @@
+/* 
+Variáves
+
+- Let -> Pode alterar o valor quando quiser
+- Const -> Valor não pode ser declarado NOVAMENTE
+- Var -> DESCONTINUADO / NÃO USAR
+
+
+*/
+
+let abacate = 30
+abacate = 20
