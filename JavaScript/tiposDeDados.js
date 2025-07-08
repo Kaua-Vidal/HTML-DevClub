8ac2fd6446b787f012c52ab578f185db3d1dc6f5
Kaua-Vidal <kaua.vidal@dcx.ufpb.br>
2025-07-07 22:55:32 -0300

    learning about data types

---
+++ JavaScript/tiposDeDados.js
@@ -0,0 +1,33 @@
+/*
+TIPOS DE DADOS
+
+1. Strings
+2. Objetos
+3. Null & Undefined
+*/
+
+
+//TEMPLATE STRINGS
+
+const myAge = 30
+const myString = `Minha idade é ${myAge}` //Template String
+
+
+//2. OBJETOS
+const name = 'Rodolfo'
+const age = 30
+const address = 'Rua dos bobos, numero 0'
+
+const rodolfo = {
+    name: 'Rodolfo',
+    age: 30,
+    address: {
+        street: 'Dos bobos',
+        number: 0,
+        city: 'São Paulo',
+        country: 'Brasil'
+    }
+}
+
+//3. NULL & UNDEFINED
+
