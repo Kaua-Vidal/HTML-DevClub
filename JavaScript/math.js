5ed5194afbf86ddacc420ab70839c6ef4badb3ea
Kaua-Vidal <kaua.vidal@dcx.ufpb.br>
2025-07-15 00:44:41 -0300

    Testing and learning about MATH and RANDOM

---
+++ JavaScript/math.js
@@ -0,0 +1,11 @@
+const result = Math.pow(2,3) // Vem de "power", que seria Potencia. 2 elevado a 3
+
+const result2 = Math.sqrt(25,2) //Raiz quadrada de 25
+
+const result3 = Math.PI //Ele irá mostrar o valor de pi
+
+const result4 = Math.ceil(3.14) //Arredonda para cima (teto)
+
+const result5 = Math.floor(3.14) //Arredonda para baixo (chão)
+
+const result6 = Math.random() //Vai trazer um numero aleatório entre 0 e 1
