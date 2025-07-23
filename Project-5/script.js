e28d93d45abc7494c047d8cd0273014db7021e5f
Kaua-Vidal <kaua.vidal@dcx.ufpb.br>
2025-07-23 00:15:35 -0300

    Project Rock, Paper, Scissors

---
+++ Project-5/script.js
@@ -0,0 +1,36 @@
+const result = document.querySelector('.result')
+const humanScore = document.querySelector('#human-score')
+const machineScore = document.querySelector('#machine-score')
+
+let humanScoreNumber = 0
+let machineScoreNumber =
+
+const playHuman = (humanChoice) => {
+    playTheGame(humanChoice, playMachine())
+}
+
+const playMachine = () => {
+    const choice = ['rock', 'paper', 'scissors']
+    const randomNumber = Math.floor(Math.random() * 3)
+    return choice[randomNumber]
+}
+
+const playTheGame = (human, machine) => {
+    if (human === machine) {
+
+        console.log('Humano: ' + human + ' Maquina: ' + machine)
+        result.innerHTML = 'Deu empate!'
+    } else if (
+        human === 'paper' && machine === 'rock' ||
+        human === 'rock' && machine === 'scissors' || 
+        human === 'scissors' && machine === 'paper') {
+
+        result.innerHTML = 'Você ganhou!'
+        humanScoreNumber++
+        humanScore.innerHTML = humanScoreNumber
+    } else {
+        machineScoreNumber++
+        machineScore.innerHTML = machineScoreNumber
+        result.innerHTML = 'Você perdeu para a Alexa!'
+    }
+}
