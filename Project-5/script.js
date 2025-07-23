e32060b57ab03a7b9ef0b800d0d5cf749b6e9675
Kaua-Vidal <kaua.vidal@dcx.ufpb.br>
2025-07-23 00:21:44 -0300

    Learning and adding ENUM

--- Project-5/script.js
+++ Project-5/script.js
@@ -5,12 +5,18 @@ const machineScore = document.querySelector('#machine-score')
 let humanScoreNumber = 0
 let machineScoreNumber = 0
 
+const GAME_OPTIONS = {
+    ROCK: 'rock',
+    PAPER: 'paper',
+    SCISSORS: 'scissors'
+}
+
 const playHuman = (humanChoice) => {
     playTheGame(humanChoice, playMachine())
 }
 
 const playMachine = () => {
-    const choice = ['rock', 'paper', 'scissors']
+    const choice = [GAME_OPTIONS.ROCK, GAME_OPTIONS.PAPER, GAME_OPTIONS.SCISSORS]
     const randomNumber = Math.floor(Math.random() * 3)
     return choice[randomNumber]
 }
@@ -21,9 +27,9 @@ const playTheGame = (human, machine) => {
         console.log('Humano: ' + human + ' Maquina: ' + machine)
         result.innerHTML = 'Deu empate!'
     } else if (
-        human === 'paper' && machine === 'rock' ||
-        human === 'rock' && machine === 'scissors' || 
-        human === 'scissors' && machine === 'paper') {
+        human === GAME_OPTIONS.PAPER && machine === GAME_OPTIONS.ROCK ||
+        human === GAME_OPTIONS.ROCK && machine === GAME_OPTIONS.SCISSORS || 
+        human === GAME_OPTIONS.SCISSORS && machine === GAME_OPTIONS.PAPER) {
 
         result.innerHTML = 'Você ganhou!'
         humanScoreNumber++
