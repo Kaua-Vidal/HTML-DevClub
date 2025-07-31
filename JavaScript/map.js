c58eb4ce652cf449c5bcb9c25eb6a67b0b8dc3f2
Kaua-Vidal <kauakauavidalpc@gmail.com>
2025-07-31 16:53:45 -0300

    Learning about Map and Reduce in JS

---
+++ JavaScript/map.js
@@ -0,0 +1,19 @@
+const students = [
+    { name: 'Rodolfo', testGrade: 7 },
+    { name: 'Maria', testGrade: 5 },
+    { name: 'João', testGrade: 8 },
+    { name: 'Bruno', testGrade: 9 },
+    { name: 'Carla', testGrade: 3 },
+    { name: 'Ana', testGrade: 2 },
+    { name: 'Julio', testGrade: 10 }
+]
+
+const newList = students.map(student => {
+    return {
+        name: student.name,
+        testGrade: student.testGrade,
+        status: student.testGrade >= 7 ? 'Aprovado' : 'Reprovado'
+    }
+})
+
+console.log(newList)
